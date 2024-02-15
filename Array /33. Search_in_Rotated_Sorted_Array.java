02d5dd4907a7c2545a374d6f18b8e76bb29d3a9c
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-02-15 20:43:08 +0530

    Create 33. Search_in_Rotated_Sorted_Array.java

---
+++ Array /33. Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            }
+
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
