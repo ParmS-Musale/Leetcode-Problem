6710c7ba86d40db3a87fd57fd5ea501316e546de
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-02-22 21:16:48 +0530

    Create 709. To_Lower_Case.java

---
+++ String /709. To_Lower_Case.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: s = "here"
+// Output: "here"
+// Example 3:
+
+// Input: s = "LOVELY"
+// Output: "lovely"
