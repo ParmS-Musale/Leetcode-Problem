ec98028bb53eff2ec16fb22f480f277528dd3b47
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-13 19:37:29 +0530

    Create 442. Find_All_Duplicates_in_an_Array.java

---
+++ 442. Find_All_Duplicates_in_an_Array.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] < 0)
+                result.add(index + 1);
+
+            nums[index] = -nums[index]; // Mark the index as visited by negating the value
+        }
+
+        return result;
+    }
+}
+
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [2,3]
+// Example 2:
+
+// Input: nums = [1,1,2]
+// Output: [1]
+// Example 3:
+
+// Input: nums = [1]
+// Output: []
+ 
