f7f5fb28b4ee07d39dca2406525a366bbade47c0
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-15 11:00:52 +0530

    Create 238. Product_of_Array_Except_Self.java

---
+++ Array /238. Product_of_Array_Except_Self.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+}
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
