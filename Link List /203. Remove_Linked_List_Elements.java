69b874389ede32990975c791225bcf94bb8801ea
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-16 10:34:28 +0530

    Create 203. Remove_Linked_List_Elements.java

---
+++ Link List /203. Remove_Linked_List_Elements.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode (-1);
+        dummy.next = head ;
+
+        ListNode curr = dummy ;
+        while (curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr= curr.next;
+            }
+        }
+            return dummy.next;
+    }
+}
+
+// Example 1:
+
+
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+// Example 2:
+
+// Input: head = [], val = 1
+// Output: []
+// Example 3:
+
+// Input: head = [7,7,7,7], val = 7
+// Output: []
