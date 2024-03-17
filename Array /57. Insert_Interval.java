22b7e4035541d162031db0eb5e861fc2d9bf8430
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-17 10:45:48 +0530

    Create 57. Insert_Interval.java

---
+++ Array /57. Insert_Interval.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        LinkedList<int[]> result = new LinkedList<>();
+        int i = 0;
+
+        // Add all intervals that end before the new interval starts
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Merge overlapping intervals
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        // Add remaining intervals
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+
+// Example 1:
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+// Example 2:
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
