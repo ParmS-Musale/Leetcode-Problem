3e95a4b03f39563acf4bfba291410acb85c51fef
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-23 11:54:21 +0530

    Create 143. Reorder_List.java

---
+++ Link List /143. Reorder_List.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // Step 1: Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        slow.next = null;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Step 3: Interleave the nodes of the first half and the reversed second half
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while (p1 != null && p2 != null) {
+            ListNode nextP1 = p1.next;
+            ListNode nextP2 = p2.next;
+
+            p1.next = p2;
+            p2.next = nextP1;
+
+            p1 = nextP1;
+            p2 = nextP2;
+        }
+    }
+}
+
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4]
+// Output: [1,4,2,3]
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [1,5,2,4,3]
