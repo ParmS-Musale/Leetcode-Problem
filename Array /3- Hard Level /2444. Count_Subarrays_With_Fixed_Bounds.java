f08f800a8afa9fb9981fe476494eb3587a6b5822
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-03-31 18:43:58 +0530

    Create 2444. Count_Subarrays_With_Fixed_Bounds.java

---
+++ Array /3- Hard Level /2444. Count_Subarrays_With_Fixed_Bounds.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                badIdx = i;
+            }
+
+            if (nums[i] == minK) {
+                leftIdx = i;
+            }
+
+            if (nums[i] == maxK) {
+                rightIdx = i;
+            }
+
+            res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+        }
+
+        return res;
+    }
+}
+
+// Example 1:
+
+// Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+// Output: 2
+// Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+// Example 2:
+
+// Input: nums = [1,1,1,1], minK = 1, maxK = 1
+// Output: 10
+// Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
