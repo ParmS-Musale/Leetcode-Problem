493b6969d08fde2df27e55a6420048f51fcfb4c8
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-04-13 20:30:50 +0530

    Create 85. Maximal_Rectangle.java

---
+++ Dynamic Programming /3 -Hard Level /85. Maximal_Rectangle.java
@@ -0,0 +1,55 @@
+class Solution {
+int result = 0;
+private void helper(int[] row, int start, int maxWidth){
+    int minHeight = row[start];
+    for(int j = start; j < row.length; j++){
+        if(row[j] == 0) return;
+        if(row[j] < minHeight) minHeight = row[j];
+        if(result >= minHeight * maxWidth)
+            // pruning
+            return;
+        int newArea = (j - start + 1) * minHeight;
+        if(result < newArea) result = newArea;
+    }
+}
+public int maximalRectangle(char[][] Mat) {
+    int n = Mat.length;
+    int m = Mat[0].length;
+    result = 0;
+    int[][] M = new int[n][m];
+    int[] prev = M[n - 1];
+    // preparing DP array
+    for(int j = 0; j < m; j++)
+        if(Mat[n - 1][j] == '1')
+            prev[j] = 1;
+    for(int i = n - 2; i > -1; i--){
+        for(int j = m - 1; j > -1; j--)
+            if(Mat[i][j] != '0')
+                M[i][j] = prev[j] + 1;
+        prev = M[i];
+    }
+    for(int i = 0; i < n; i++){
+        int maxHeight = n - i;
+        for(int j = 0; j < m; j++){
+            int maxWidth =  m - j;
+            if(result >= maxHeight * maxWidth)
+                // pruning
+                continue;
+            helper(M[i], j, maxWidth);
+        }
+    }
+    return result;
+}
+}
+
+// Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+// Output: 6
+// Explanation: The maximal rectangle is shown in the above picture.
+// Example 2:
+
+// Input: matrix = [["0"]]
+// Output: 0
+// Example 3:
+
+// Input: matrix = [["1"]]
+// Output: 1
