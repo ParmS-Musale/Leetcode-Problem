3920aa9ff475606b8ecedefa4dda2dd54098e3ea
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-05-25 10:35:27 +0530

    Create 140. Word_Break_II.java

---
+++ Hash Table /3 - Hard Level /140. Word_Break_II.java
@@ -0,0 +1,44 @@
+class Solution {
+   public List<String> wordBreak(String s, List<String> wordDict) {  
+       HashMap<Integer, List<String>> hm = new HashMap<>(); 
+       HashSet<String> hs = new HashSet<>(wordDict);
+       return wordBreakHelper(s, 0, hs, hm);
+}
+
+private List<String> wordBreakHelper(String s, int start, HashSet<String> dict, HashMap<Integer, List<String>> hm ) {
+
+    if (hm.containsKey(start))
+        return hm.get(start); 
+
+    List<String> validSubstr = new ArrayList<>();
+
+    if (start == s.length())
+        validSubstr.add("");
+
+    for (int end = start + 1; end <= s.length(); end++) {
+        String prefix = s.substring(start, end);
+        if (dict.contains(prefix)) {
+            List<String> suffixes = wordBreakHelper(s, end, dict, hm);
+            for (String suffix : suffixes)
+               validSubstr.add(prefix + (suffix.equals("") ? "" : " ") + suffix); 
+        }
+    }
+ 
+    hm.put(start, validSubstr);
+    return validSubstr;
+ }
+}
+
+// Example 1:
+
+// Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+// Output: ["cats and dog","cat sand dog"]
+// Example 2:
+
+// Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+// Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+// Explanation: Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: []
