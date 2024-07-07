64583110e42bd98269e6dffb9c42ee6b707e2536
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-07-07 15:13:18 +0530

    Create 328. Odd_Even_Linked_List.java

---
+++ Link List /2- Medium Level /328. Odd_Even_Linked_List.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        // Optimal Aproach
+
+        // Edge Case
+
+        if(head == null || head.next == null){
+            return head ;
+        }
+
+        ListNode odd = head ;
+        ListNode even = head.next ;
+        ListNode evenHead = head.next ;
+
+        while (even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        } 
+        // connection point
+        odd.next = evenHead ;
+        return head;
+    }
+}
+
+
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [1,3,5,2,4]
+// Example 2:
+
+
+// Input: head = [2,1,3,5,6,4,7]
+// Output: [2,3,6,7,1,5,4]
